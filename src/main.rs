4ec63a53db2591c554392833776dbb9c86d5548b
Alexander Chelyadinov <artma.gronsky@gmail.com>
2023-09-09 11:34:22 +0300

    D2[MINOR]:  Add clip lib, update byte size in console row.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, Result, Read, Write, ErrorKind};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
+fn main() -> Result<()>{
     let silent = !env::var("PV_SILENT").unwrap_or(String::new()).is_empty();
 
     let mut total_bytes = 0;
@@ -12,14 +12,23 @@ fn main() {
         let num_read: usize = match io::stdin().read(&mut buffer) {
             Ok(num) if num == 0 => break,
             Ok(num) => num,
-            Err(_) => break,
+            Err(e) => return Err(e),
         };
 
         total_bytes += num_read;
 
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if !silent {
+            eprint!("\r total_bytes: {}", total_bytes);
+        }
+
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]){
+            if e.kind() == ErrorKind::BrokenPipe{
+                break;
+            }
+            eprintln!("Oh no, an error! {}", e);
+            std::process::exit(1);
+        };
     }
-    if !silent {
-        eprintln!("total_bytes: {}", total_bytes);
-    }
+ 
+    Ok(())
 }
