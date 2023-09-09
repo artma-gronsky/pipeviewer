b6e446f52c6ab41ada589c168637236928ebd9a8
Alexander Chelyadinov <artma.gronsky@gmail.com>
2023-09-09 12:25:23 +0300

    D2[MINOR]: Use clap to parse cmd line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
+use clap::Arg;
 use std::env;
-use std::io::{self, Result, Read, Write, ErrorKind};
+use std::io::{self, ErrorKind, Read, Result, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() -> Result<()>{
-    let silent = !env::var("PV_SILENT").unwrap_or(String::new()).is_empty();
+fn main() -> Result<()> {
+    let matches = clap::Command::new("pipeviewer")
+        .bin_name("pipeviewer")
+        //.arg_required_else_help(true)
+        .args([
+            Arg::new("infile").long("infile").help("Read from a file instead of stdin"),
+            Arg::new("outfile")
+                .short('o')
+                .long("outfile")
+                .help("Whrite output to a file instead of stdout"),
+        ])
+        .arg(Arg::new("silent").short('s').long("silent"))
+        .get_matches();
+
+    let infile = matches
+        .get_one::<String>("infile")
+        .map(|s| s.to_owned())
+        .unwrap_or_default();
+    let outfile = matches
+        .get_one::<String>("outfile")
+        .map(|s| s.to_owned())
+        .unwrap_or_default();
+
+    let silent = if matches.contains_id("silent") {
+        true
+    } else {
+        !env::var("PV_SILENT").unwrap_or(String::new()).is_empty()
+    };
+
+    dbg!(infile, outfile, silent);
 
     let mut total_bytes = 0;
     let mut buffer = [0; CHUNK_SIZE];
@@ -21,14 +50,14 @@ fn main() -> Result<()>{
             eprint!("\r total_bytes: {}", total_bytes);
         }
 
-        if let Err(e) = io::stdout().write_all(&buffer[..num_read]){
-            if e.kind() == ErrorKind::BrokenPipe{
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
                 break;
             }
-            eprintln!("Oh no, an error! {}", e);
+            eprintln!("\n Oh no, an error! {}", e);
             std::process::exit(1);
         };
     }
- 
+
     Ok(())
 }
